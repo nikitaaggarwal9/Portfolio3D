0a05eaf69fa0d23316dc164bfcf4e744d1368f09
Nikita <nikitaaggarwal6600@gmail.com>
2023-05-21 18:24:09 +0530

    Hero section - 3D computer added

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { Html, useProgress } from "@react-three/drei";
 
-const Loader = () => {
+const CanvasLoader = () => {
+  const { progress } = useProgress();
   return (
-    <div>Loader</div>
-  )
-}
+    <Html
+      as='div'
+      center
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
 
-export default Loader
+
+
+    >
+      <span className='canvas-loader'></span>
+      <p
+        style={{
+          fontSize: 14,
+          color: "#F1F1F1",
+          fontWeight: 800,
+          marginTop: 40,
+        }}
+      >
+        {progress.toFixed(2)}%
+      </p>
+    </Html>
+  );
+};
+
+export default CanvasLoader;
